924dccec3cd5b66983112c631c84f346e0695e40
Mohsen <nasirimohsen16@gmail.com>
2019-11-30 01:27:33 +0330

    ;/

--- Minecraft Fisher/Fisher.py
+++ Minecraft Fisher/Fisher.py
@@ -4,7 +4,7 @@ import numpy as np
 from skimage import feature
 from sklearn.externals import joblib
 import os
-from time import sleep
+from time import sleep,time
 import win32api, win32con
 
 def click():
@@ -32,41 +32,46 @@ model = joblib.load('Fish.pkl')
 
 mins = np.array([0,200,0])
 maxs = np.array([10,255,255])
+sleep(3)
 while True:
-    screen = np.array(ImageGrab.grab(bbox=(int(xs),int(ys),int(xe),int(ye))))
+    sttime = time()
+    click()
+    caught = False
+    while ((time() - sttime) < 45):
+        screen = np.array(ImageGrab.grab(bbox=(int(xs),int(ys),int(xe),int(ye))))
 
-    frame = cv.cvtColor(screen,cv.COLOR_BGR2RGB)
+        frame = cv.cvtColor(screen,cv.COLOR_BGR2RGB)
 
-    hsv = cv.cvtColor(frame,cv.COLOR_RGB2HSV)
-    binaried = cv.inRange(hsv,mins,maxs)
-    filtered = cv.bitwise_and(frame,frame,mask = binaried)
+        hsv = cv.cvtColor(frame,cv.COLOR_RGB2HSV)
+        binaried = cv.inRange(hsv,mins,maxs)
+        filtered = cv.bitwise_and(frame,frame,mask = binaried)
 
-    img = cv.resize(filtered,(SX,SY))
-    Fvec = feature.hog(img) 
+        img = cv.resize(filtered,(SX,SY))
+        Fvec = feature.hog(img) 
 
-    score = model.predict_proba(np.array(Fvec.reshape(1, -1)))
-    for smt in score[0]:
-        scorelist.append(percent(smt))
-    if max(scorelist) < avgscore:
-        print("Unknown: ",max(scorelist))    
-    else:
-        result = model.predict(np.array(Fvec).reshape(1, -1))
-        if result[0] == 'n':
-            checker = []
+        score = model.predict_proba(np.array(Fvec.reshape(1, -1)))
+        for smt in score[0]:
+            scorelist.append(percent(smt))
+        if max(scorelist) < avgscore:
+            print("Unknown: ",max(scorelist))    
         else:
-            checker.append('c')
-            if len(checker) >= Ncheck:
-                print("Will draw back in one sec")
-                sleep(1.5)
-                click()
-                print("Will throw back in two sec")
-                sleep(2)
-                click()
+            result = model.predict(np.array(Fvec).reshape(1, -1))
+            if result[0] == 'n':
+                checker = []
+            else:
+                checker.append('c')
+                if len(checker) >= Ncheck:
+                    print("Will draw back in one sec")
+                    sleep(1.9)
+                    click()
+                    caught = True
+                    break
 
+            print(result[0])
 
-        print(result[0])
-
-    cv.imshow('window',filtered)
-    if cv.waitKey(1) == ord('q'):
-        cv.destroyAllWindows()
-        break
+        cv.imshow('window',filtered)
+        if cv.waitKey(1) == ord('q'):
+            cv.destroyAllWindows()
+            break
+    if caught != True:
+        click()
